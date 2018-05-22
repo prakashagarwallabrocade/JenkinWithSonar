0e4329f8ca7aa4f7f2f9fd74c0a851dbe128be89
prakash kumar agarwalla <pagarwa@lowes.com>
2018-05-22 18:10:30 +0530

    new

---
+++ MathOperations/src/main/java/math/operation/ArithmeticOperations.java
@@ -0,0 +1,14 @@
+package math.operation;
+
+public class ArithmeticOperations {
+	
+
+	public Integer substract(Integer a, Integer b)
+	{
+		return a-b;
+	}
+	public Integer add(Integer a, Integer b)
+	{
+		return a+b;
+	}
+}
