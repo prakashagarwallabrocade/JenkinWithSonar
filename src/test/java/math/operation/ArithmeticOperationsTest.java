0e12590914f353af5f183de82c6c5dcf100ee97e
prakash kumar agarwalla <pagarwa@lowes.com>
2018-05-28 11:58:41 +0530

    updated

--- src/test/java/math/operation/ArithmeticOperationsTest.java
+++ src/test/java/math/operation/ArithmeticOperationsTest.java
@@ -5,17 +5,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ArithmeticOperationsTest {
-	
 
 	@Test
-	public void testAdd()
-	{
-	
+	public void testSubstract() {
+
+		ArithmeticOperations operations = new ArithmeticOperations();
+		
+		Integer actual = operations.substract(10, 6);
+		Integer expected = 4;
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	public void testAdd() {
+
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.add(2, 6);
 		Integer expected = 8;
 		assertEquals(expected, actual);
-		
+
 	}
 
 }
